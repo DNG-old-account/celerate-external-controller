46e904f863bb057792f7b18cb5df05cca2c41dd6
Barath Raghavan <barath@icsi.berkeley.edu>
2014-08-11 22:29:59 -0700

    Updates initial json files to basic set needed to populate database.

--- dbinit/initdb.sh
+++ dbinit/initdb.sh
@@ -1,6 +1,6 @@
 ~/arangodb/bin/arangoimp --create-collection true --collection hardware --file hardware.json
 ~/arangodb/bin/arangoimp --create-collection true --collection node --file node.json
-~/arangodb/bin/arangoimp --create-collection true --collection profile --file profile.json
+~/arangodb/bin/arangoimp --create-collection true --collection plan --file plan.json
 ~/arangodb/bin/arangoimp --create-collection true --collection role --file role.json
 ~/arangodb/bin/arangoimp --create-collection true --collection topology --file topology.json
 ~/arangodb/bin/arangoimp --create-collection true --collection user --file user.json
