41bd6b88f7c7bc9b2ff3f7e46a9936f8ceedfcc3
Barath Raghavan <barath@icsi.berkeley.edu>
2014-09-02 11:18:44 -0700

    Updated initdb with hardware.

--- dbinit/initdb.sh
+++ dbinit/initdb.sh
@@ -1,3 +1,4 @@
 mongoimport -h localhost:3001 --db meteor --collection nodes --jsonArray icinga-hosts.json
 mongoimport -h localhost:3001 --db meteor --collection subscribers --jsonArray export-google-form-8-25.json
 mongoimport -h localhost:3001 --db meteor --collection subscribers --jsonArray insightly-aug-18.json
+mongoimport -h localhost:3001 --db meteor --collection hardware --jsonArray hardware.json
