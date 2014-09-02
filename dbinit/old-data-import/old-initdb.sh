fce1b97c58861dcc0e3d21a7ca9d750118cd3b97
Barath Raghavan <barath@icsi.berkeley.edu>
2014-09-02 10:52:20 -0700

    Sets up meteor-oriented data importing.

---
+++ dbinit/old-data-import/old-initdb.sh
@@ -0,0 +1,7 @@
+arangoimp --create-collection true --collection hardware --file hardware.json
+arangoimp --create-collection true --collection node --file node.json
+arangoimp --create-collection true --collection plan --file plan.json
+arangoimp --create-collection true --collection role --file role.json
+arangoimp --create-collection true --collection topology --file topology.json
+arangoimp --create-collection true --collection user --file user.json
+arangoimp --create-collection true --collection subscriber --file subscriber.json
