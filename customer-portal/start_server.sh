ae1c4a7014f80e1978a07de6dd4682341d3ca9bf
= <=>
2014-10-03 02:01:48 +0000

    Fixing production urls - this time for real.

--- customer-portal/start_server.sh
+++ customer-portal/start_server.sh
@@ -4,7 +4,7 @@ MONGO_URL="mongodb://localhost:3001/meteor"
 if [[ "$1" == "dev" ]]; then 
   ROOT_URL="http://localhost:3002"
 else
-  ROOT_URL="http://celerate-controller:3002"
+  ROOT_URL="http://celerate-controller.furtherreach.net:3002"
 fi
 
 meteor -p 3002 --settings ../settings.js 
--- meteor-controller/start_server.sh
+++ meteor-controller/start_server.sh
@@ -3,7 +3,7 @@
 if [[ "$1" == "dev" ]]; then 
   ROOT_URL="http://localhost:3002"
 else
-  ROOT_URL="http://celerate-controller:3000"
+  ROOT_URL="http://celerate-controller.furtherreach.net:3000"
 fi
 
 meteor -p 3000 --settings ../settings.js 
