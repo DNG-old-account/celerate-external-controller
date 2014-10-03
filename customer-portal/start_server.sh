f2400fe8e9be7aff01f70995217e02e46e7f3e10
Barath Raghavan <barath@icsi.berkeley.edu>
2014-10-03 08:55:26 -0700

    Fixed ports and added echo output.

--- customer-portal/start_server.sh
+++ customer-portal/start_server.sh
@@ -3,8 +3,10 @@ export MONGO_URL="mongodb://localhost:3001/meteor"
 
 if [[ "$1" == "dev" ]]; then 
   export ROOT_URL="http://localhost:3002"
+  echo "Starting celerate customer portal in dev mode at: " $ROOT_URL
 else
   export ROOT_URL="http://celerate-controller.furtherreach.net:3002"
+  echo "Starting celerate customer portal in prod mode at: " $ROOT_URL
 fi
 
 meteor -p 3002 --settings ../settings.js 
--- meteor-controller/start_server.sh
+++ meteor-controller/start_server.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [[ "$1" == "dev" ]]; then 
-  export ROOT_URL="http://localhost:3002"
+  export ROOT_URL="http://localhost:3000"
+  echo "Starting main celerate controller in dev mode at: " $ROOT_URL
 else
   export ROOT_URL="http://celerate-controller.furtherreach.net:3000"
+  echo "Starting main celerate controller in prod mode at: " $ROOT_URL
 fi
 
 meteor -p 3000 --settings ../settings.js 
