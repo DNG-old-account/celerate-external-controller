a75835938184c64d8c89dfc66cf47f018b892975
= <=>
2014-10-03 01:52:05 +0000

    Updating start server scripts.

--- customer-portal/start_server.sh
+++ customer-portal/start_server.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
-export MONGO_URL="mongodb://localhost:3001/meteor"
+#!/bin/bash
+MONGO_URL="mongodb://localhost:3001/meteor"
+
+if [[ "$1" == "dev" ]]; then 
+  ROOT_URL="http://localhost:3002"
+else
+  ROOT_URL="http://celerate-website.furtherreach.net:3002"
+fi
+
 meteor -p 3002 --settings ../settings.js 
--- meteor-controller/start_server.sh
+++ meteor-controller/start_server.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+if [[ "$1" == "dev" ]]; then 
+  ROOT_URL="http://localhost:3002"
+else
+  ROOT_URL="http://celerate-website.furtherreach.net:3000"
+fi
 
 meteor -p 3000 --settings ../settings.js 
