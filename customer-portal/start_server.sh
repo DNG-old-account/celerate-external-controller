d82d3fd382be1a588bb8ca4de99d7ad1b08cb664
Max B <max@denovogroup.org>
2014-10-27 17:06:01 -0400

    Modifying start scripts for staging server.

--- customer-portal/start_server.sh
+++ customer-portal/start_server.sh
@@ -4,6 +4,9 @@ export MONGO_URL="mongodb://localhost:3001/meteor"
 if [[ "$1" == "dev" ]]; then 
   export ROOT_URL="http://localhost:3002"
   echo "Starting celerate customer portal in dev mode at: " $ROOT_URL
+elif [[ "$1" == "staging" ]]; then
+  export ROOT_URL="http://staging.furtherreach.net:3002"
+  echo "Starting celerate customer portal in staging mode at: " $ROOT_URL
 else
   export ROOT_URL="https://celerate-controller.furtherreach.net"
   echo "Starting celerate customer portal in prod mode at: " $ROOT_URL
--- meteor-controller/start_server.sh
+++ meteor-controller/start_server.sh
@@ -3,6 +3,9 @@
 if [[ "$1" == "dev" ]]; then 
   export ROOT_URL="http://localhost:3000"
   echo "Starting main celerate controller in dev mode at: " $ROOT_URL
+elif [[ "$1" == "staging" ]]; then
+  export ROOT_URL="http://staging.furtherreach.net:3000"
+  echo "Starting main celerate controller in staging mode at: " $ROOT_URL
 else
   export ROOT_URL="http://celerate-controller.furtherreach.net:3000"
   echo "Starting main celerate controller in prod mode at: " $ROOT_URL
