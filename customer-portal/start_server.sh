a6fba733faf7db09fd74ae9810fd50088ddbf10d
Maxb <maxb.personal@gmail.com>
2015-05-30 22:25:09 +0000

    Setting to Meteor to run in production mode where appropriate.

--- customer-portal/start_server.sh
+++ customer-portal/start_server.sh
@@ -2,6 +2,8 @@
 export MONGO_URL="mongodb://localhost:3001/meteor"
 export MONGO_OPLOG_URL="mongodb://localhost:3001/local"
 
+EXTRA_PARAMS=""
+
 if [[ "$1" == "dev" ]]; then 
   export ROOT_URL="http://localhost:3002"
   echo "Starting celerate customer portal in dev mode at: " $ROOT_URL
@@ -10,7 +12,8 @@ elif [[ "$1" == "staging" ]]; then
   echo "Starting celerate customer portal in staging mode at: " $ROOT_URL
 else
   export ROOT_URL="https://portal.furtherreach.net"
+  EXTRA_PARAMS="$EXTRA_PARAMS --production"
   echo "Starting celerate customer portal in prod mode at: " $ROOT_URL
 fi
 
-meteor -p 3002 --settings ../settings.js 
+meteor $EXTRA_PARAMS -p 3002 --settings ../settings.js 
--- meteor-controller/start_server.sh
+++ meteor-controller/start_server.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export MONGO_OPLOG_URL="mongodb://localhost:3001/local"
 
+EXTRA_PARAMS=""
+
 if [[ "$1" == "dev" ]]; then 
   export ROOT_URL="http://localhost:3000"
   echo "Starting main celerate controller in dev mode at: " $ROOT_URL
@@ -9,7 +11,8 @@ elif [[ "$1" == "staging" ]]; then
   echo "Starting main celerate controller in staging mode at: " $ROOT_URL
 else
   export ROOT_URL="http://celerate-controller.furtherreach.net:3000"
+  EXTRA_PARAMS="$EXTRA_PARAMS --production"
   echo "Starting main celerate controller in prod mode at: " $ROOT_URL
 fi
 
-meteor -p 3000 --settings ../settings.js 
+meteor $EXTRA_PARAMS -p 3000 --settings ../settings.js 
